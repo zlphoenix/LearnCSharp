6800beda0cfe022c0849a16aa5f9c2d8dea261b9
Allen Zhou <zlphoenix@gmail.com>
2013-09-11 10:54:47 +0800

    加测试结论

--- CSharpFeatures/CSharpFeaturesTest/Reflector/CustomerAttributeTest.cs
+++ CSharpFeatures/CSharpFeaturesTest/Reflector/CustomerAttributeTest.cs
@@ -5,28 +5,61 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CSharpFeaturesTest.Reflector
 {
     [AttributeUsage(AttributeTargets.Property, Inherited = false)]
-    internal class CustomerAttribute : Attribute
+    internal class CustomerNonInheritedAttribute : Attribute
+    {
+        public string Position { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = true)]
+    internal class CustomerInheritedAttribute : Attribute
     {
         public string Position { get; set; }
     }
 
     internal class Base
     {
-        [CustomerAttribute(Position = "Base")]
+        /// <summary>
+        /// 父类有而子类没有的属性,子类可以获取到它的Attrib,无论查询是是否追溯父类
+        /// </summary>
+        [CustomerNonInheritedAttribute(Position = "Base")]
         public string Code { get; set; }
 
-        [CustomerAttribute(Position = "Base")]
+        [CustomerInherited(Position = "Base")]
+        [CustomerNonInheritedAttribute(Position = "Base")]
         public virtual string Description { get; set; }
+
+        [CustomerInherited(Position = "Base")]
+        public virtual string Misc { get; set; }
+
+
+        [CustomerInherited(Position = "Base")]
+        public virtual string Property { get; set; }
 
     }
 
     internal class Derived : Base
     {
-        [CustomerAttribute(Position = "Derived")]
+        /// <summary>
+        /// 子类的标签会覆盖父类的标签
+        /// </summary>
+        [CustomerNonInheritedAttribute(Position = "Derived")]
         public string Name { get; set; }
 
-        [CustomerAttribute(Position = "Derived")]
+        /// <summary>
+        /// 父类和子类都有的属性,以子类为准
+        /// </summary>
+        [CustomerNonInheritedAttribute(Position = "Derived")]
+        [CustomerInherited(Position = "Derived")]
         public override string Description { get; set; }
+
+        /// <summary>
+        /// 子类不打标签,但标签类本身可以被继承,也会被子类查到,查询必须向父类追踪
+        /// </summary>
+        public override string Misc { get; set; }
+
+
+
+        [CustomerInherited(Position = "Derived")]
+        public override string Property { get; set; }
     }
 
     [TestClass]
@@ -35,13 +68,25 @@ namespace CSharpFeaturesTest.Reflector
         [TestMethod]
         public void GetCustomerAttrFromDerivedClass()
         {
+
             var ps = (from p in typeof(Derived).GetProperties()
-                      where p.GetCustomAttributes(typeof(CustomerAttribute), true)
-                         .Select(o => o as CustomerAttribute)
-                         .Any(ca => ca.Position == "Derived")
+                      where p.GetCustomAttributes(typeof(CustomerNonInheritedAttribute), false)
+                         .Any(o => o is CustomerNonInheritedAttribute)
                       select p).ToList();
-            Assert.AreEqual(2, ps.Count(), "父类中的Attrib没有取到");
-           
+
+            Assert.AreEqual(3, ps.Count());
+
+
+            ps = (from p in typeof(Derived).GetProperties()
+                  where p.GetCustomAttributes(typeof(CustomerInheritedAttribute), false)
+                     .Any(o => o is CustomerInheritedAttribute)
+                  select p).ToList();
+
+            //不包含没有
+            Assert.IsFalse(ps.Any(p => p.Name == "Misc"));
+
+            //Assert.AreEqual(3, ps.Count());
+
         }
     }
 }
