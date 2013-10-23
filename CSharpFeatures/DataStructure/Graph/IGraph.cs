a15301a75286bbfeba6ab9506098b9b4d875d83b
Allen Zhou <zlphoenix@gmail.com>
2013-10-23 23:33:56 +0800

    图算法，图基本操作

--- CSharpFeatures/DataStructure/Graph/IGraph.cs
+++ CSharpFeatures/DataStructure/Graph/IGraph.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using DataStructure.Common;
 
 namespace DataStructure.Graph
@@ -12,7 +11,7 @@ namespace DataStructure.Graph
         /// <summary>
         /// 顶点数组
         /// </summary>
-        Node<T>[] Nodes { get; }
+        List<Node<T>> Nodes { get; }
         /// <summary>
         /// 边的数目 
         /// </summary>
@@ -21,32 +20,46 @@ namespace DataStructure.Graph
         /// 邻接矩阵数组 
         /// </summary>
         int[,] Matrix { get; }
+    }
 
-
+    public class GraphFactory<TData>
+    {
+        public static T Create<T>(Type t, int n)
+        {
+            return (T) Activator.CreateInstance(t, n);
+        }
     }
 
     /// <summary>
-    /// 邻接矩阵无向图
+    /// 邻接矩阵图
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class GraphAdjMatrix<T> : IGraph<T>
+    public abstract class GraphAdjMatrixBase<T>
     {
-        #region propterties
-
-        public Node<T>[] Nodes { get; private set; }
-        public int NumEdges { get; private set; }
-        public int[,] Matrix { get; private set; }
-        #endregion
-        /// <summary>
-        /// 构造一个定点个数为n的无向图
-        /// </summary>
-        /// <param name="n">定点个数</param>
-        public GraphAdjMatrix(int n)
+        protected GraphAdjMatrixBase(int n)
         {
-            Nodes = new Node<T>[n];
+            Nodes = new List<Node<T>>(n);
             Matrix = new int[n, n];
             NumEdges = 0;
         }
+        public List<Node<T>> Nodes { get; protected set; }
+        public int NumEdges { get; protected set; }
+        public int[,] Matrix { get; protected set; }
+
+
+        /// <summary>
+        /// 判断顶点v1与v2之间是否存在边
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public bool IsEdge(Node<T> v1, Node<T> v2)
+        {
+            int x, y;
+            GetPoint(v1, v2, out x, out y);
+            return Matrix[x, y] != 0;
+        }
+
         /// <summary>
         /// 判断v是否是图的顶点 
         /// </summary>
@@ -57,6 +70,7 @@ namespace DataStructure.Graph
             //遍历顶点数组 
             return Nodes.Contains(v);
         }
+
         /// <summary>
         /// 添加一条V1到v2的边，其权值为v
         /// </summary>
@@ -71,6 +85,38 @@ namespace DataStructure.Graph
             Matrix[x, y] = v;
             //Matrix[GetIndex(v2), GetIndex(v1)] = v;
             ++NumEdges;
+        }
+
+        public void SetEdge(T v1, T v2, int v)
+        {
+            var p1 = GetNode(v1);
+            var p2 = GetNode(v2);
+            SetEdge(p1, p2, v);
+        }
+
+        /// <summary>
+        /// 获取边的权值
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public int GetEdge(T v1, T v2)
+        {
+            var p1 = GetNode(v1);
+            var p2 = GetNode(v2);
+            return GetEdge(p1, p2);
+        }
+        /// <summary>
+        /// 获取边的权值
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public int GetEdge(Node<T> v1, Node<T> v2)
+        {
+            int x, y;
+            GetPoint(v1, v2, out x, out y);
+            return Matrix[x, y];
         }
         /// <summary>
         /// 删除顶点v1和v2之间的边 
@@ -89,18 +135,105 @@ namespace DataStructure.Graph
                 --NumEdges;
             }
         }
+        private Node<T> GetNode(T v)
+        {
+            var result = Nodes.FirstOrDefault(n => n.Data.Equals(v));
+            if (result == null) throw new Exception(string.Format("Node {0} not found!", v));
+            return result;
+        }
 
         /// <summary>
-        /// 判断顶点v1与v2之间是否存在边
+        /// 求入度
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public virtual int GetInDegree(T v)
+        {
+            //矩阵第i列的不为0的边的条数
+            var nodeIndex = Nodes.IndexOf(GetNode(v));
+            var count = 0;
+            for (var i = 0; i < Nodes.Count; i++)
+            {
+                if (Matrix[i, nodeIndex] != 0) count++;
+            }
+            return count;
+
+        }
+        /// <summary>
+        /// 取出度
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public virtual int GetOutDegree(T v)
+        {
+            //矩阵第i行的不为0的边的条数
+            var nodeIndex = Nodes.IndexOf(GetNode(v));
+            var count = 0;
+            for (var i = 0; i < Nodes.Count; i++)
+            {
+                if (Matrix[nodeIndex, i] != 0) count++;
+            }
+            return count;
+
+        }
+        /// <summary>
+        /// 获取定点坐标
         /// </summary>
         /// <param name="v1"></param>
         /// <param name="v2"></param>
-        /// <returns></returns>
-        public bool IsEdge(Node<T> v1, Node<T> v2)
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        protected abstract void GetPoint(Node<T> v1, Node<T> v2, out int x, out int y);
+
+        /// <summary>
+        /// 输出矩阵
+        /// </summary>
+        public void Print()
         {
-            int x, y;
-            GetPoint(v1, v2, out x, out y);
-            return Matrix[x, y] != 0;
+            for (var i = 0; i <= Nodes.Count; i++)
+            {
+                if (i == 0)
+                {
+                    PrintTittle();
+                    continue;
+                }
+                for (var j = 0; j <= Nodes.Count; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write(Nodes[i - 1].Data + "\t");
+                        continue;
+                    }
+
+                    Console.Write(Matrix[i - 1, j - 1] + "\t");
+                    if (j == Nodes.Count) Console.Write("\n");
+                }
+            }
+        }
+
+        private void PrintTittle()
+        {
+            var tittle = new StringBuilder("\t", Nodes.Count * 4);
+            Nodes.ForEach(node => tittle.Append(node.Data.ToString() + "\t"));
+            //string title = 
+            Console.WriteLine(tittle);
+        }
+    }
+
+    /// <summary>
+    /// 邻接矩阵无向图
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GraphAdjMatrix<T> : GraphAdjMatrixBase<T>, IGraph<T>
+    {
+        /// <summary>
+        /// 构造一个定点个数为n的无向图
+        /// </summary>
+        /// <param name="n">定点个数</param>
+        public GraphAdjMatrix(int n)
+            : base(n)
+        {
+
         }
 
         /// <summary>
@@ -114,6 +247,7 @@ namespace DataStructure.Graph
             y = x - y;
             x -= y;
         }
+
         /// <summary>
         /// 获取定点坐标
         /// </summary>
@@ -121,7 +255,7 @@ namespace DataStructure.Graph
         /// <param name="v2"></param>
         /// <param name="x"></param>
         /// <param name="y"></param>
-        private void GetPoint(Node<T> v1, Node<T> v2, out int x, out int y)
+        protected override void GetPoint(Node<T> v1, Node<T> v2, out int x, out int y)
         {
             //v1或v2不是图的顶点 
             if (!IsNode(v1) || !IsNode(v2))
@@ -130,8 +264,8 @@ namespace DataStructure.Graph
             }
             //顶点v1与v2之间存在边 
 
-            x = Array.IndexOf(Nodes, v1);
-            y = Array.IndexOf(Nodes, v2);
+            x = Nodes.IndexOf(v1);
+            y = Nodes.IndexOf(v2);
             if (x == y) throw new Exception("需要建立边的两个定点不可以是同一个定点");
             //矩阵是对称矩阵,只记录上三角矩阵，
             if (x > y)
@@ -140,6 +274,54 @@ namespace DataStructure.Graph
             }
         }
 
+        public override int GetInDegree(T v)
+        {
+            return GetDegree(v);
+        }
 
+        public override int GetOutDegree(T v)
+        {
+            return GetDegree(v);
+        }
+
+        private int GetDegree(T v)
+        {
+            return base.GetInDegree(v) + base.GetOutDegree(v);
+        }
+
+    }
+    /// <summary>
+    /// 邻接矩阵有向图
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DirectedGraphAdjMatrix<T> : GraphAdjMatrixBase<T>
+    {
+        /// <summary>
+        /// 构造一个定点个数为n的无向图
+        /// </summary>
+        /// <param name="n">定点个数</param>
+        public DirectedGraphAdjMatrix(int n)
+            : base(n) { }
+
+        /// <summary>
+        /// 获取定点坐标
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        protected override void GetPoint(Node<T> v1, Node<T> v2, out int x, out int y)
+        {
+            //v1或v2不是图的顶点 
+            if (!IsNode(v1) || !IsNode(v2))
+            {
+                throw new Exception("Node is not belong to Graph!");
+            }
+            //顶点v1与v2之间存在边 
+            x = Nodes.IndexOf(v1);
+            y = Nodes.IndexOf(v2);
+            if (x == y) throw new Exception("需要建立边的两个定点不可以是同一个定点");
+
+        }
     }
 }
---
+++ CSharpFeatures/DataStructureTest/GraphTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructure.Common;
+using DataStructure.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureTest
+{
+    [TestClass]
+    public class GraphTest
+    {
+        [TestMethod]
+        public void CreateGraph()
+        {
+            var graph = new GraphAdjMatrix<string>(5);
+            var vertex = new List<Node<string>>
+                {
+                    new Node<string>("A"),
+                    new Node<string>("B"),
+                    new Node<string>("C"),
+                    new Node<string>("D"),
+                    new Node<string>("E"),
+                };
+            graph.Nodes.AddRange(vertex);
+
+            graph.SetEdge("A", "B", 60);
+            graph.SetEdge("A", "C", 100);
+            graph.SetEdge("A", "D", 20);
+            graph.SetEdge("B", "C", 80);
+            graph.SetEdge("B", "D", 95);
+            graph.SetEdge("C", "E", 70);
+            graph.SetEdge("D", "E", 10);
+
+            graph.Print();
+        }
+
+    }
+}
