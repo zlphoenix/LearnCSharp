5413ee87a9448fc8ef5d8fa314ebe11d29034a9b
Allen Zhou <zlphoenix@gmail.com>
2013-11-14 16:37:19 +0800

    DynamicTypeTest

---
+++ CSharpFeatures/CSharpFeaturesTest/Dynamic/DynamicType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpFeaturesTest.Dynamic
+{
+    [TestClass]
+    public class DynamicType
+    {
+        [TestMethod]
+        public void ValueTypeInDynamic()
+        {
+            var i = 10;
+            dynamic d = i;
+            d++;
+            i = d;//动态类型可以为强类型赋值
+            Assert.AreEqual(i, d);
+
+            dynamic dObj = new DType();
+            dObj.Order = 10;
+            dObj.Order++;
+            Assert.AreEqual(i, dObj.Order);
+
+        }
+    }
+
+    public class DType
+    {
+        public int Order;
+    }
+}
---
+++ CSharpFeatures/CSharpFeaturesTest/Reflector/GenericTypeTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CSharpFeaturesTest.Reflector
+{
+    [TestClass]
+    public class GenericTypeTest
+    {
+        [TestMethod]
+        public void GetTypeFromTypeName()
+        {
+            var ps = new List<Persion>();
+            var t = ps.GetType();
+            Console.WriteLine(t);
+
+            Console.WriteLine(t.FullName);
+
+            var t2 = Type.GetType(t.FullName);
+            Assert.AreEqual(t, t2);
+
+            Console.WriteLine(typeof(Dictionary<string, Persion>).FullName);
+            var pa1 = new Persion[0];
+            var pa2 = new Persion[10];
+
+
+            var t3 = Type.GetType(pa1.GetType().FullName);
+            Console.WriteLine(t3.FullName);
+            var t4 = Type.GetType(pa2.GetType().FullName);
+            Console.WriteLine(t4.FullName);
+        }
+    }
+
+    public class Persion
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
