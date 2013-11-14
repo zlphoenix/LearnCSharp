09a0f04ff6d69fd78e0f836195c415fb3f875edb
Allen Zhou <zlphoenix@gmail.com>
2013-11-14 16:46:23 +0800

    Vs2013 new Feature

---
+++ CSharpFeatures/VS2013OneWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VS2013OneWebApp.Startup))]
+namespace VS2013OneWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
