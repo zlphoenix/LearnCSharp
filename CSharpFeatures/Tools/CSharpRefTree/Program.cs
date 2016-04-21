0a70ba3cf1454ef15b333b5fdbd4c025f78b605d
Allen Zhou <zlphoenix@gmail.com>
2016-04-21 21:43:00 +0800

    by Dll: duplicated, Ref not found, build order, recursive ref

--- CSharpFeatures/Tools/CSharpRefTree/Program.cs
+++ CSharpFeatures/Tools/CSharpRefTree/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 
 namespace Allen.Util.CSharpRefTree
 {
@@ -9,58 +11,278 @@ namespace Allen.Util.CSharpRefTree
     /// </summary>
     static class Program
     {
+        public static Dictionary<string, PrjInfo> prjInfoDic;
+        public static Dictionary<string, PrjInfo> prjInfoFileNameDic;
+        public static List<PrjInfo> errorPrjInfo;
+        public static List<PrjInfo> root;
+        public static List<string> AssemblyPath;
+        public static List<string> LostAssembly = new List<string>();
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Console.WriteLine(":)");
-
-
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
 
-            var prjFileNames = EverythingFileSearcher.Searcher.Search(@"D:\Work\GSP\GSP6.1\Draft\Src,*.csproj");
-            var prjDic = new Dictionary<string, PrjInfo>();
-            var prjFileNameDic = new Dictionary<string, PrjInfo>();
-
-            var refErrorPrj = new List<PrjInfo>();
-            var root = new List<PrjInfo>();
-            foreach (var prjInfo in prjFileNames.Select(CreatePrjInfo))
+            AssemblyPath = EverythingFileSearcher.Searcher.Search(@"D:\Work\GSP\GSP6.1\Draft\Ref\bin !D:\Work\GSP\GSP6.1\Draft\Ref\bin\3rd (.dll|.exe)").ToList();
+            Console.WriteLine("Total file count {0} ", AssemblyPath.Count);
+            prjInfoDic = new Dictionary<string, PrjInfo>();
+            prjInfoFileNameDic = new Dictionary<string, PrjInfo>();
+            errorPrjInfo = new List<PrjInfo>();
+            root = new List<PrjInfo>();
+            AssemblyPath.ForEach(assPath =>
             {
-                prjDic.Add(prjInfo.AssemblyName, prjInfo);
-                prjFileNameDic.Add(prjInfo.PrjFileName, prjInfo);
-                if (prjInfo.OriginalRef.Count == 0) root.Add(prjInfo);
+                if (!assPath.EndsWith("dll") && !assPath.EndsWith("exe")) return;
+                CreatePrjInfo(assPath);
+            }
+            );
+            Console.WriteLine("Total aas count {0}", prjInfoDic.Count);
+            RebuildRef();
+            PrintAss();
+        }
+
+        private static void PrintAss()
+        {
+            foreach (var prjInfo in prjInfoDic.Values.Where(item => item.RefError.Count > 0))
+            {
+                Console.WriteLine(prjInfo.ToString());
             }
 
-            foreach (var prjInfo in prjDic.Values)
-            {
-                var succeed = BuildRef(prjInfo);
-                if (!succeed)
-                    refErrorPrj.Add(prjInfo);
+            Console.WriteLine(LostAssembly.OutputList("Lost Assemblies"));
 
+            int level = 0;
+            Console.WriteLine(root.OutputList($"-------------Level {level}-------------\n", item => item.AssemblyName + "\n"));
+            root.OrderBy(item => item.Module).ToList().ForEach(item => prjInfoDic.Remove(item.AssemblyName));
+            level++;
+            while (prjInfoDic.Count > 0)
+            {
+                var count = prjInfoDic.Count;
+
+
+                var thisLevel = prjInfoDic.Values
+                    .Where(item => (!item.PrjRef.Exists(refItem => !root.Contains(refItem))))
+                    .OrderBy(item => item.Module)
+                    .ToList();
+                Console.WriteLine(thisLevel.OutputList($"-------------Level {level}-------------\n", item => item.AssemblyName + "\n"));
+                root.AddRange(thisLevel);
+                thisLevel.ForEach(item => prjInfoDic.Remove(item.AssemblyName));
+
+                if (count == prjInfoDic.Count)
+                {
+                    DealRecursiveRef();
+                    if (count == prjInfoDic.Count)
+                    {
+                        Console.WriteLine("Failed to remove cycle!");
+                        break;
+                    }
+                }
+                level++;
+            }
+
+            Console.WriteLine(prjInfoDic.Values.ToList().OrderBy(item => item.Module).ToList().OutputList("-------------Not Reachable-------------\n"
+            , item => item.AssemblyName + "\n"));
+            foreach (var notReachableAss in prjInfoDic)
+            {
+                Console.WriteLine(
+              notReachableAss.Value.PrjRef
+                    .Where(item => !root.Contains(item))
+                    .Select(item => item.AssemblyName)
+                    .ToList()
+                    .OutputList(notReachableAss.Key + " Ref not in builded list\n", item => "\t" + item + "\n")
+                    );
             }
 
         }
 
-        private static bool BuildRef(PrjInfo prjInfo)
+        private static void DealRecursiveRef()
         {
-            throw new NotImplementedException();
+            var path = new Stack<PrjInfo>(prjInfoDic.Count);
+            foreach (var prjInfo in prjInfoDic.Values.ToList())
+            {
+                //内部会打断循环引用，会影响到集合的成员
+                if (root.Contains(prjInfo))
+                    continue;
+                if (Deal(prjInfo, path))
+                {
+                    path.Clear();
+                }
+            }
+        }
+        /// <summary>
+        /// 深度优先探测环
+        /// </summary>
+        /// <param name="prjInfo"></param>
+        /// <param name="path"></param>
+        public static bool Deal(PrjInfo prjInfo, Stack<PrjInfo> path)
+        {
+            foreach (var refAssembly in prjInfo.PrjRef.Where(item => (!root.Contains(item))))
+            {
+
+                if (path.Contains(refAssembly))
+                {
+                    //发现环
+                    Console.Write(refAssembly.AssemblyName + "<-");
+                    while (path.Peek() != refAssembly)
+                    {
+                        Console.Write(path.Pop().AssemblyName + "<-");
+                    }
+                    Console.Write(refAssembly.AssemblyName + "\t Resolve it?[Y/n]\n");
+                    //var decide = Console.ReadLine();
+                    //if (decide == "Y")
+                    //{
+                    root.Add(refAssembly);
+                    prjInfoDic.Remove(refAssembly.AssemblyName);
+                    return true;
+                    //}
+                }
+                path.Push(refAssembly);
+                if (Deal(refAssembly, path)) return true;
+                path.Pop();
+
+            }
+            return false;
+        }
+        private static void RebuildRef()
+        {
+            foreach (var prjInfo in prjInfoDic.Values)
+            {
+                if (prjInfo.OriginalRef.Count == 0)
+                    continue;
+
+                foreach (var refAssName in prjInfo.OriginalRef)
+                {
+                    if (prjInfoDic.ContainsKey(refAssName))
+                    {
+                        var refAss = prjInfoDic[refAssName];
+                        prjInfo.PrjRef.Add(refAss);
+                        refAss.BeRefBy.Add(prjInfo);
+                    }
+                    else
+                    {
+                        if (!LostAssembly.Contains(refAssName))
+                            LostAssembly.Add(refAssName);
+                        prjInfo.RefError.Add(refAssName);
+                    }
+                }
+            }
         }
 
-        private static PrjInfo CreatePrjInfo(string prj)
+        private static void AddToDics(PrjInfo prjInfo)
         {
-            throw new NotImplementedException();
+            if (!prjInfoDic.ContainsKey(prjInfo.AssemblyName))
+            {
+                prjInfoDic.Add(prjInfo.AssemblyName, prjInfo);
+            }
+            //prjInfoFileNameDic.Add(prjInfo.PrjFileName, prjInfo);
+            if (prjInfo.OriginalRef.Count == 0) root.Add(prjInfo);
+        }
+
+
+        public static PrjInfo CreatePrjInfo(string prj)
+        {
+            string dllName = GetFileName(prj);
+            if (prjInfoDic.ContainsKey(dllName))
+            {
+
+                Console.WriteLine($"{dllName}\t{prj} load\t {prjInfoDic[dllName].AssemblyPath} before ");
+                return null; //已经添加过
+            }
+            var ass = Assembly.ReflectionOnlyLoadFrom(prj);
+            var prjInfo = new PrjInfo(ass);
+            prjInfo.AssemblyPath = prj;
+            AddToDics(prjInfo);
+
+            return prjInfo;
+        }
+
+        private static string GetFileName(string prj)
+        {
+            var fileName = prj.Split('\\').LastOrDefault();
+            return fileName.Substring(0, fileName.Length - 4);
         }
     }
 
     internal class PrjInfo
     {
-        public string AssemblyName { get; set; }
+        public PrjInfo()
+        {
+            PrjRef = new List<PrjInfo>();
+            OriginalRef = new List<string>();
+            RefError = new List<string>();
+            BeRefBy = new List<PrjInfo>();
+        }
+
+        public PrjInfo(Assembly ass) : this()
+        {
+
+            this.AssemblyName = ass.GetName().Name;
+
+            var refAssemblies = ass.GetReferencedAssemblies().Where(IsMatch);
+            if (refAssemblies.Count() == 0) return;
+            foreach (var refAss in refAssemblies)
+            {
+                this.OriginalRef.Add(refAss.Name);
+            }
+
+
+        }
+
+        private PrjInfo LoadAssByRefName(AssemblyName refAss)
+        {
+            var assPath = Program.AssemblyPath.FirstOrDefault(path => path.Contains(refAss.Name));
+            if (string.IsNullOrEmpty(assPath))
+            {
+                this.RefError.Add($"Ref：{assPath} not found ");
+                return null;
+            }
+            else
+            {
+                return Program.CreatePrjInfo(assPath);
+
+            }
+        }
+
+        public static bool IsMatch(AssemblyName assemblyName)
+        {
+            var excludeNames = new string[] { "mscorlib", "DevExpress", "Newtonsoft" };
+            if (excludeNames.Any(item => assemblyName.Name.StartsWith(item))
+                || assemblyName.Name.StartsWith("System") || assemblyName.Name.StartsWith("Microsoft")) return false;
+            if (assemblyName.Name.StartsWith("Inspur") || assemblyName.Name.StartsWith("Genersoft") || assemblyName.Name.Contains("GSP")) return true;
+
+
+            //Console.WriteLine("AssName：{0} Could Not be recognized", assemblyName.Name);
+            return false;
+        }
+
+        public string Module { get; set; }
+        private string assemblyName;
+
+        public string AssemblyName
+        {
+            get { return assemblyName; }
+            set
+            {
+                assemblyName = value;
+                if (assemblyName.StartsWith("Inspur") || assemblyName.StartsWith("Genersoft"))
+                {
+                    Module = assemblyName.Split('.')[2];
+                }
+                //else if (assemblyName.StartsWith("Genersoft"))
+                //{
+                //    Module = assemblyName.Split('.')[2]
+                //}
+            }
+        }
+
+
         public string PrjFileName { get; set; }
         public string PrjFilePath { get; set; }
+        public string AssemblyPath { get; set; }
         /// <summary>
         /// 项目引用
         /// </summary>
@@ -73,5 +295,42 @@ namespace Allen.Util.CSharpRefTree
         /// 错误的引用
         /// </summary>
         public List<string> RefError { get; set; }
+
+        public List<PrjInfo> BeRefBy { get; set; }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"Ass:{AssemblyName};" +
+                   //$"\t{OriginalRef.OutputList("Ref")};" +
+                   $"\t{RefError.OutputList("Error")}";
+        }
+        public string ToString(string str)
+        {
+            return $"Ass:{AssemblyName};" +
+                   $"\t{OriginalRef.OutputList("Ref:")};" +
+                   $"\t{RefError.OutputList("Error: ")}" +
+                   "\n";
+        }
     }
+
+    public static class Util
+    {
+        public static StringBuilder OutputList<T>(this List<T> collection, string title, Func<T, string> print = null)
+        {
+            if (print == null)
+                print = arg => arg.ToString() + ",";
+            if (collection.Count == 0) return null;
+            var refs = new StringBuilder(title);
+
+            collection.ForEach(item => refs.Append(print(item)));
+            return refs;
+        }
+    }
+
 }
--- CSharpFeatures/Tools/EverythingFileSearcher/Searcher.cs
+++ CSharpFeatures/Tools/EverythingFileSearcher/Searcher.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -7,87 +6,110 @@ namespace Allen.Util.EverythingFileSearcher
 {
     public class Searcher
     {
-        const int EVERYTHING_OK = 0;
-        const int EVERYTHING_ERROR_MEMORY = 1;
-        const int EVERYTHING_ERROR_IPC = 2;
-        const int EVERYTHING_ERROR_REGISTERCLASSEX = 3;
-        const int EVERYTHING_ERROR_CREATEWINDOW = 4;
-        const int EVERYTHING_ERROR_CREATETHREAD = 5;
-        const int EVERYTHING_ERROR_INVALIDINDEX = 6;
-        const int EVERYTHING_ERROR_INVALIDCALL = 7;
+        private const int EVERYTHING_OK = 0;
+        private const int EVERYTHING_ERROR_MEMORY = 1;
+        private const int EVERYTHING_ERROR_IPC = 2;
+        private const int EVERYTHING_ERROR_REGISTERCLASSEX = 3;
+        private const int EVERYTHING_ERROR_CREATEWINDOW = 4;
+        private const int EVERYTHING_ERROR_CREATETHREAD = 5;
+        private const int EVERYTHING_ERROR_INVALIDINDEX = 6;
+        private const int EVERYTHING_ERROR_INVALIDCALL = 7;
 
-        [DllImport("Everything64.dll", CharSet = CharSet.Unicode)]
+        [DllImport("Everything32.dll", CharSet = CharSet.Unicode)]
         public static extern int Everything_SetSearchW(string lpSearchString);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern void Everything_SetMatchPath(bool bEnable);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern void Everything_SetMatchCase(bool bEnable);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern void Everything_SetMatchWholeWord(bool bEnable);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern void Everything_SetRegex(bool bEnable);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern void Everything_SetMax(int dwMax);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern void Everything_SetOffset(int dwOffset);
 
-        [DllImport("Everything64.dll")]
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_GetMatchPath();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_GetMatchCase();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_GetMatchWholeWord();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_GetRegex();
-        [DllImport("Everything64.dll")]
-        public static extern UInt32 Everything_GetMax();
-        [DllImport("Everything64.dll")]
-        public static extern UInt32 Everything_GetOffset();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
+        public static extern uint Everything_GetMax();
+
+        [DllImport("Everything32.dll")]
+        public static extern uint Everything_GetOffset();
+
+        [DllImport("Everything32.dll")]
         public static extern string Everything_GetSearchW();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern int Everything_GetLastError();
 
-        [DllImport("Everything64.dll")]
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_QueryW(bool bWait);
 
-        [DllImport("Everything64.dll")]
+        [DllImport("Everything32.dll")]
         public static extern void Everything_SortResultsByPath();
 
-        [DllImport("Everything64.dll")]
+        [DllImport("Everything32.dll")]
         public static extern int Everything_GetNumFileResults();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern int Everything_GetNumFolderResults();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern int Everything_GetNumResults();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern int Everything_GetTotFileResults();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern int Everything_GetTotFolderResults();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern int Everything_GetTotResults();
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_IsVolumeResult(int nIndex);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_IsFolderResult(int nIndex);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern bool Everything_IsFileResult(int nIndex);
-        [DllImport("Everything64.dll", CharSet = CharSet.Unicode)]
+
+        [DllImport("Everything32.dll", CharSet = CharSet.Unicode)]
         public static extern void Everything_GetResultFullPathNameW(int nIndex, StringBuilder lpString, int nMaxCount);
-        [DllImport("Everything64.dll")]
+
+        [DllImport("Everything32.dll")]
         public static extern void Everything_Reset();
 
 
         /// <summary>
-        /// 使用Everything执行文件搜索
+        ///     使用Everything执行文件搜索
         /// </summary>
         /// <param name="queryStr"></param>
         /// <returns></returns>
-        public static IList<string> Search(string queryStr)
+        public static IList<string> Search(string queryStr, bool isSortBypath = true)
         {
             int i;
             const int bufsize = 260;
-            StringBuilder buf = new StringBuilder(bufsize);
+            var buf = new StringBuilder(bufsize);
 
             // set the search
             Everything_SetSearchW(queryStr);
@@ -99,9 +121,11 @@ namespace Allen.Util.EverythingFileSearcher
             // execute the query
             Everything_QueryW(true);
 
+            if (isSortBypath)
+                Everything_SortResultsByPath();
+
             var count = Everything_GetNumResults();
             var result = new List<string>(count);
-
 
 
             // loop through the results, adding each result to the listbox.
