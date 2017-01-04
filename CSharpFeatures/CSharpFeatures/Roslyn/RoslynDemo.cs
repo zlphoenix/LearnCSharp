5a87f2fe5f51ce1468cb851e7d3d16375e2502ed
Allen <Allen@BLACKROSE>
2017-01-04 20:12:04 +0800

    add roslyn ref

---
+++ CSharpFeatures/CSharpFeatures/Roslyn/RoslynDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFeatures.Roslyn
+{
+    public class RoslynDemo
+    {
+        //static void Main(string[] args)
+        //{
+        //    var options =
+        //        ScriptOptions.Default
+        //       .AddReferences("System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
+
+        //    var bar = new Bar() { StaffId = 5686, UnitId = 2, Age = 15 };
+        //    Microsoft.CodeAnalysis.CSharp.Scripting.CSharpScript.RunAsync("System.Console.WriteLine(\"hello world\");", options);
+
+        //}
+    }
+}
