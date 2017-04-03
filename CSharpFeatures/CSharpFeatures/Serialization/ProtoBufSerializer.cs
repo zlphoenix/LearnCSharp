6f73bb279f45b16ef7dfbdbc8226eca2a49e6164
Allen <Allen@BLACKROSE>
2017-04-03 11:30:05 +0800

    Protobuf,disposable

---
+++ CSharpFeatures/CSharpFeatures/Serialization/ProtoBufSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf.Meta;
+
+namespace CSharpFeatures.Serialization
+{
+
+    public class ProtoBufSerializer
+    {
+        public static List<T> Do<T>(List<T> list)
+        {
+            using (var ms = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize(ms, list);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                var result = ProtoBuf.Serializer.Deserialize<List<T>>(ms);
+
+                //var mt = RuntimeTypeModel.Default.Add(typeof(Model), true);
+                return result;
+            }
+        }
+
+    }
+
+    public class Model
+    {
+        public string Code { get; set; }
+        public List<string> Detail { get; set; }
+    }
+
+}
