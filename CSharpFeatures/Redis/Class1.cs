40c899ff1b30f291d065c3b8685e4309ff7e5c81
Allen Zhou <zlphoenix@gmail.com>
2017-06-23 00:49:04 +0800

    add init roslyn analysis prj

---
+++ CSharpFeatures/Redis/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allen.Redis
+{
+    public class RedisClient
+    {
+
+    }
+}
